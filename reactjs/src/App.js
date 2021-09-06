a8f721f8f91f7aabe25f69402fcc22afe567f329
sitaram kumar29 <sitharam.pamidi@mobigesuture.com>
2021-09-06 18:26:25 +0530

    Adding User data card component using fetch method with in the component. And resolve page horizontal scroll issue.

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -4,6 +4,7 @@ import BasicDetails from "./views/BasicDetails";
 import UsernameContext from "./context/UsernameContext";
 import Username from "./components/Username";
 import { useState } from "react";
+import UserDataCard from "./views/userdatacard";
 
 function App() {
   const [username, setUsername] = useState("saisyam");
@@ -12,9 +13,10 @@ function App() {
   return (
     <UsernameContext.Provider value={value}>
       <NavBar></NavBar>
-      <Container fluid style={{ "padding": "10px" }}>
+      <Container fluid style={{ "padding-top": "10px" }}>
         <Username></Username>
         <BasicDetails></BasicDetails>
+        <UserDataCard></UserDataCard>
       </Container>
     </UsernameContext.Provider>
   );
